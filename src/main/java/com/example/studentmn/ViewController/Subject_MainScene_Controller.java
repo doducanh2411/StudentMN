9a10718758c6de45f202bc3cb5eeead2d9fee215
Duc Anh <doducanh2411@gmail.com>
2023-05-11 11:49:09 +0700

    Fix something

--- src/main/java/com/example/studentmn/ViewController/Subject_MainScene_Controller.java
+++ src/main/java/com/example/studentmn/ViewController/Subject_MainScene_Controller.java
@@ -629,19 +629,19 @@ public class Subject_MainScene_Controller implements Initializable,ViewTeacher {
                 alert.setHeaderText(null);
                 alert.setContentText("Please select all blank fields");
                 alert.showAndWait();
-            } else if (!getComponentPoint.getText().matches("/^(10(\\.0{1,2})?|[0-9](\\.[0-9]{1,2})?)$/gm")) {
+            } else if (!getComponentPoint.getText().matches("^(10(\\.0{1,2})?|[0-9](\\.[0-9]{1,2})?)$")) {
                 alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error message");
                 alert.setHeaderText(null);
                 alert.setContentText("Invalid component point!");
                 alert.showAndWait();
-            } else if (!getMidPoint.getText().matches("/^(10(\\.0{1,2})?|[0-9](\\.[0-9]{1,2})?)$/gm")) {
+            } else if (!getMidPoint.getText().matches("^(10(\\.0{1,2})?|[0-9](\\.[0-9]{1,2})?)$")) {
                 alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error message");
                 alert.setHeaderText(null);
                 alert.setContentText("Invalid mid point!");
                 alert.showAndWait();
-            } else if (!getEndPoint.getText().matches("/^(10(\\.0{1,2})?|[0-9](\\.[0-9]{1,2})?)$/gm")) {
+            } else if (!getEndPoint.getText().matches("^(10(\\.0{1,2})?|[0-9](\\.[0-9]{1,2})?)$")) {
                 alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error message");
                 alert.setHeaderText(null);
